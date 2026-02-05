1820f0e070dd9fa447a71133ad48beab36b08a1b
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-02-05 16:56:15 +0700

    Add: CRUD

--- index.js
+++ index.js
@@ -13,19 +13,68 @@ app.get('/', (req, res) => {
 })
 
 app.get('/my-laptop', (req, res) => {
-    res.render('my-laptop')
+    res.render('my-laptop', { laptopData })
 })
 
 app.get('/contact-us', (req, res) => {
     res.render('contact-us')
 })
 
-app.post('/kirim', (req, res) => {
-    const coba = req.body.text;
-    console.log('Masuk mas ' + coba)
-    res.redirect('/?input=' + coba)
-})
+app.post('/my-laptop', addLaptop)
+
+app.post('/my-laptop/:id', deleteLaptop)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+let laptopData = [
+    {
+        id: 1,
+        namaLaptop: "Yanto LED",
+        tanggalBeli: "2020-05-06",
+        deskripsi: "Bisa menyala",
+        kondisi: {
+            monitorBagus: true,
+            keyboardBagus: false,
+            casingBagus: false,
+        }
+    },
+    {
+        id: 2,
+        namaLaptop: "Agus Monitor",
+        tanggalBeli: "2022-05-06",
+        deskripsi: "Keyboard menyala",
+        kondisi: {
+            monitorBagus: false,
+            keyboardBagus: true,
+            casingBagus: false,
+        }
+    },
+]
+let idLaptop = 3
+
+function addLaptop(req, res) {
+    const { namaLaptop, tanggalBeli, deskripsi, monitorBagus, keyboardBagus, casingBagus } = req.body;
+    const laptop = {
+        id: idLaptop++,
+        namaLaptop: namaLaptop,
+        tanggalBeli: tanggalBeli,
+        deskripsi: deskripsi,
+        kondisi: {
+            monitorBagus: monitorBagus === 'option1' ? true : false,
+            keyboardBagus: keyboardBagus === 'option2' ? true : false,
+            casingBagus: casingBagus === 'option3' ? true : false,
+        }
+    };
+
+    laptopData.push(laptop);
+    console.log(laptopData);
+    res.redirect('/my-laptop');
+}
+
+function deleteLaptop(req, res) {
+    const id = parseInt(req.params.id);
+    laptopData = laptopData.filter((laptop) => laptop.id !== id);
+    res.redirect('/my-laptop');
+}
