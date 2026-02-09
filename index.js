389fae2622726200cc316a1e6cc1f58680a88530
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-02-09 17:16:00 +0700

    Add: CRUD dan connect Database

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import { Pool } from 'pg'
+
+const db = new Pool({
+    user: 'postgres',
+    password: 'alifsufim',
+    host: 'localhost',
+    port: 5432,
+    database: 'personal-web-65',
+    max: 20,
+})
+
 const app = express()
 const port = 3000
 
@@ -12,69 +23,109 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/my-laptop', (req, res) => {
-    res.render('my-laptop', { laptopData })
-})
+app.get('/my-laptop', dataLaptop)
+
+app.post('/my-laptop', addLaptop)
+
+app.get('/my-laptop/edit/:id', getLaptop)
+
+app.post('/my-laptop/update', updateLaptop)
+
+app.post('/my-laptop/delete/:id', deleteLaptop)
 
 app.get('/contact-us', (req, res) => {
     res.render('contact-us')
 })
 
-app.post('/my-laptop', addLaptop)
-
-app.post('/my-laptop/:id', deleteLaptop)
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-let laptopData = [
-    {
-        id: 1,
-        namaLaptop: "Yanto LED",
-        tanggalBeli: "2020-05-06",
-        deskripsi: "Bisa menyala",
-        kondisi: {
-            monitorBagus: true,
-            keyboardBagus: false,
-            casingBagus: false,
-        }
-    },
-    {
-        id: 2,
-        namaLaptop: "Agus Monitor",
-        tanggalBeli: "2022-05-06",
-        deskripsi: "Keyboard menyala",
-        kondisi: {
-            monitorBagus: false,
-            keyboardBagus: true,
-            casingBagus: false,
-        }
-    },
-]
-let idLaptop = 3
+// Display Data Laptop
+async function dataLaptop(req, res) {
+    try {
+        const query = "SELECT *, TO_CHAR(tanggal_beli, 'DD-MM-YYYY') AS tanggal_rapi FROM laptops ORDER BY id ASC";
+        const result = await db.query(query);
 
-function addLaptop(req, res) {
-    const { namaLaptop, tanggalBeli, deskripsi, monitorBagus, keyboardBagus, casingBagus } = req.body;
-    const laptop = {
-        id: idLaptop++,
-        namaLaptop: namaLaptop,
-        tanggalBeli: tanggalBeli,
-        deskripsi: deskripsi,
-        kondisi: {
+        res.render('my-laptop', { result })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+// Display edit Data Laptop by id
+async function getLaptop(req, res) {
+    try {
+        const id = req.params.id;
+
+        const query = `SELECT id, nama, TO_CHAR(tanggal_beli, 'YYYY-MM-DD') AS tanggal_input, deskripsi, monitor_bagus, keyboard_bagus, casing_bagus FROM laptops where id= ${id}`;
+        const result = await db.query(query);
+        // console.log(result.rows);
+
+        res.render('my-laptop-edit', { result })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+// Update Data Laptop by id
+async function updateLaptop(req, res) {
+    try {
+        const { id, namaLaptop, tanggalBeli, deskripsi, monitorBagus, keyboardBagus, casingBagus } = req.body;
+        const laptop = {
+            id: id,
+            namaLaptop: namaLaptop,
+            tanggalBeli: tanggalBeli,
+            deskripsi: deskripsi,
             monitorBagus: monitorBagus === 'option1' ? true : false,
             keyboardBagus: keyboardBagus === 'option2' ? true : false,
             casingBagus: casingBagus === 'option3' ? true : false,
-        }
-    };
+        };
 
-    laptopData.push(laptop);
-    console.log(laptopData);
-    res.redirect('/my-laptop');
+        const query = `UPDATE laptops SET nama='${laptop.namaLaptop}', tanggal_beli='${laptop.tanggalBeli}', deskripsi='${laptop.deskripsi}', monitor_bagus=${laptop.monitorBagus}, keyboard_bagus=${laptop.keyboardBagus}, casing_bagus=${laptop.casingBagus} WHERE id=${laptop.id}`;
+        const result = await db.query(query);
+        console.log(result);
+        res.redirect('/my-laptop')
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
 }
 
-function deleteLaptop(req, res) {
-    const id = parseInt(req.params.id);
-    laptopData = laptopData.filter((laptop) => laptop.id !== id);
-    res.redirect('/my-laptop');
+// Add Data Laptop
+async function addLaptop(req, res) {
+    try {
+        const { namaLaptop, tanggalBeli, deskripsi, monitorBagus, keyboardBagus, casingBagus } = req.body;
+        const laptop = {
+            namaLaptop: namaLaptop,
+            tanggalBeli: tanggalBeli,
+            deskripsi: deskripsi,
+            monitorBagus: monitorBagus === 'option1' ? true : false,
+            keyboardBagus: keyboardBagus === 'option2' ? true : false,
+            casingBagus: casingBagus === 'option3' ? true : false,
+        };
+        const query = `INSERT INTO laptops (nama,tanggal_beli,deskripsi,user_id,monitor_bagus,keyboard_bagus,casing_bagus) VALUES ('${laptop.namaLaptop}','${laptop.tanggalBeli}','${laptop.deskripsi}',1,${laptop.monitorBagus},${laptop.keyboardBagus},${laptop.casingBagus})`;
+        const result = await db.query(query);
+        console.log(result);
+        res.redirect('/my-laptop');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+// Delete Data Laptop
+async function deleteLaptop(req, res) {
+    try {
+        const id = req.params.id;
+        const query = `DELETE FROM laptops WHERE id=${id}`;
+        const result = await db.query(query);
+        console.log(result);
+        res.redirect('/my-laptop');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error")
+    }
 }
